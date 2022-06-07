227e64c08ed8302a5aab07c40cf0dfc500f1b8b1
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-07 22:55:14 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function receivesAFunction(callBackFunc){
+    callBackFunc()
+ }
+
+
+function returnsANamedFunction(){
+    return function randomFunction(){}
+}
+
+ function returnsAnAnonymousFunction(){
+    return function (){}
+ }
+ function receivesAFunction (callBackFunction) {
+    callBackFunction();
+}
+
+
+ function returnsANamedFunction () {
+     return function aNamedFunction () {};
+ }
+
+ function returnsAnAnonymousFunction () {
+     return function () {};
+ } 
